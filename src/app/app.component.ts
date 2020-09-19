4a0ff2cb221c3bd5170ba029e372c0513f6e3eb7
BarbaraRM <bromeromachare@gmail.com>
2020-09-18 20:54:10 -0500

    version1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practica2';
+  title = 'Practica de Laboratorio 2';
+  public autor = "Barbara Romero";
+  public mostrar_autor = false;
+  
+  public mostrar_lista = true;
+  public lista_de_usuarios = [ 'Barbara Romero', 'Paulo Ramirez', 'Henry Pardo'];
+
+  public texto_boton = "Ocultar lista";  
+  change():void{
+    this.mostrar_lista = !this.mostrar_lista;
+    if(this.mostrar_lista == false){
+      this.texto_boton = "Mostrar lista";
+    }else{
+      this.texto_boton = "Ocultar lista";
+    }
+  }
+
+  buscar() { 
+    var hay_coincidencias = false;
+    for(var i=0; i<this.lista_de_usuarios.length; i++){
+      if(this.lista_de_usuarios[i] == this.autor){
+        hay_coincidencias = true;
+      }
+    }
+    if(hay_coincidencias==true){
+      alert('Si hay coincidencias con la búsqueda');
+    }else{
+      alert('No hay coincidencias con la búsqueda');
+    }
+  }
 }
+
+
